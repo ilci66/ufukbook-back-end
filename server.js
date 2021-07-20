f26af27b5e87a4517d6bbbaf76d97662ed943e10
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-07-20 12:37:24 +0200

    cors fix

--- server.js
+++ server.js
@@ -17,12 +17,7 @@ const routes = require('./routes/routes.js')
 app.use(bodyParser.json({limit: '16mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '16mb', extended: true }))
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors({}));
 app.use(
   session({
     secret:"secretcode",
